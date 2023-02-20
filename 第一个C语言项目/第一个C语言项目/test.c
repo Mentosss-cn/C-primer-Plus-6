97c02635105f5f2ad9d2f987ad7002415cc98f6b
Mentosss <71159015+Mentosss-cn@users.noreply.github.com>
2023-02-20 16:52:32 +0800

    Add files via upload

---
+++ 第一个C语言项目/第一个C语言项目/test.c
@@ -0,0 +1,23 @@
+//写代码
+//写出主函数(main)
+//C语言的代码是从主函数的第一行开始执行的
+//所以C语言代码中得有main函数——入口
+
+//printf是一个库函数—在屏幕上打印信息
+//printf函数也得打招呼（引用头文件stdio.h）
+
+#include <stdio.h>//include包含 stdio.h头文件
+int main()//main程序的入口
+{
+	printf("Hello World\n");
+	return 0;
+};
+
+//编译+连接+运行代码
+//快捷键Ctrl+F5
+#include <stdio.h>
+int a()
+{
+	printf("比特\n");
+	return 0;
+};
